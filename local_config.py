7fd9d1e6270956a8a7136943a8153b9ce0959ab7
Barath Prathosh <barathprathosh@gmail.com>
2025-04-17 12:13:46 +0530

    Update local_config.py

--- local_config.py
+++ local_config.py
@@ -4,3 +4,9 @@ ERPNEXT_API_KEY = ''
 ERPNEXT_API_SECRET = ''
 ERPNEXT_URL = 'http://dev.local:8000'
 ERPNEXT_VERSION = 13
+
+# Database related Configs
+DB_HOST = 'localhost'
+DB_USER = 'root'
+DATABASE_NAME = 'epushserver'
+DB_PASSWORD = 'admin'
